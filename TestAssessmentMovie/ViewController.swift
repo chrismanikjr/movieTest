e794ed9b17a6b56113c430c9045623e78a8aa5fd
Chris Manik <chris.manikjr@gmail.com>
2023-10-22 21:33:17 +0700

    Initial Commit

---
+++ TestAssessmentMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAssessmentMovie
+//
+//  Created by Chrismanto Natanail Manik on 22/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
